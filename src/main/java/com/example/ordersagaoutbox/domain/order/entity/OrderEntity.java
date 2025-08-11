d8bb990e9b79ceca05b60ae2741c17ec30b285af
baegjonghyeon <baek011226@naver.com>
2025-08-11 18:54:34 +0900

    feat: - OrderStatus 업데이트 - OrderEntity 멱등키 + 상태 확장

--- src/main/java/com/example/ordersagaoutbox/domain/order/entity/OrderEntity.java
+++ src/main/java/com/example/ordersagaoutbox/domain/order/entity/OrderEntity.java
@@ -11,17 +11,28 @@ import java.util.UUID;
 @Getter
 public class OrderEntity {
     @Id private String id;
+
     @Column(nullable=false) private String productId;
     @Column(nullable=false) private int quantity;
     @Column(nullable=false) private long amount;
-    @Enumerated(EnumType.STRING) @Column(nullable=false) private OrderStatus status;
-    @Column(nullable=false) private Instant createdAt;
 
-    public static OrderEntity newOrder(String productId, int quantity, long amount){
+    @Enumerated(EnumType.STRING) @Column(nullable=false)
+    private OrderStatus status;
+
+    @Column(nullable=false, unique = true)          // 멱등키
+    private String idempotencyKey;
+
+
+    @Column(nullable=false) private Instant createdAt;
+    @Column(nullable = false) private Instant updateAt;
+
+    public static OrderEntity newOrder(String productId, int quantity, long amount, String idem){
         OrderEntity o = new OrderEntity();
         o.id = UUID.randomUUID().toString();
         o.productId = productId; o.quantity = quantity; o.amount = amount;
-        o.status = OrderStatus.NEW; o.createdAt = Instant.now();
+        o.status = OrderStatus.NEW;
+        o.idempotencyKey = idem;
+        o.createdAt = Instant.now(); o.updateAt = o.createdAt;
         return o;
     }
 }
--- src/main/java/com/example/ordersagaoutbox/domain/order/entity/OrderStatus.java
+++ src/main/java/com/example/ordersagaoutbox/domain/order/entity/OrderStatus.java
@@ -1,5 +1,9 @@
 package com.example.ordersagaoutbox.domain.order.entity;
 
 public enum OrderStatus {
-    NEW, COMPLETED, CANCELLED
+    NEW,                // 주문 생성됨
+    INVENTORY_RESERVED, // 재고 예약 완료
+    PAYMENT_AUTHORIZED, // 결제 승인 완료
+    COMPLETED,          // 주문 완료
+    CANCELLED           // 실패로 취소(보상 수행)
 }
