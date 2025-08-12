9fbaf5975a2ff102a058e0d3b986961b9dc2ad8c
baegjonghyeon <baek011226@naver.com>
2025-08-12 15:32:57 +0900

    feat: 스케줄러 활성화

--- src/main/java/com/example/ordersagaoutbox/OrderSagaOutboxApplication.java
+++ src/main/java/com/example/ordersagaoutbox/OrderSagaOutboxApplication.java
@@ -2,8 +2,10 @@ package com.example.ordersagaoutbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class OrderSagaOutboxApplication {
 
     public static void main(String[] args) {
