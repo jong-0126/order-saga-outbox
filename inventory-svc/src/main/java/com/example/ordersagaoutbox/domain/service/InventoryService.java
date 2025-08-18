6d5bfa3a3d2e8a2082eafaa9956c625e46a7389b
baegjonghyeon <baek011226@naver.com>
2025-08-18 17:35:11 +0900

    feat: - 시드 추가, 조회 로직 수정

--- inventory-svc/src/main/java/com/example/ordersagaoutbox/domain/service/InventoryService.java
+++ inventory-svc/src/main/java/com/example/ordersagaoutbox/domain/service/InventoryService.java
@@ -1,46 +1,65 @@
 package com.example.ordersagaoutbox.domain.service;
 
+import com.example.ordersagaoutbox.domain.dto.InventoryResponseDto;
 import com.example.ordersagaoutbox.domain.entity.InventoryEntity;
 import com.example.ordersagaoutbox.domain.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.Instant;
 
 @Service
 @RequiredArgsConstructor
 public class InventoryService {
-    private final InventoryRepository repo;
+    private final InventoryRepository inventoryRepository;
 
     @Transactional
-    public void seed(String productId, int qty) {
-        InventoryEntity e = repo.findById(productId)
-                .orElseGet(() -> InventoryEntity.builder().productId(productId).quantity(0).build());
-        e.setQuantity(qty);
-        e.setUpdatedAt(Instant.now());
-        repo.save(e);
+    public void seed(String productId, int qty){
+        InventoryEntity inventoryEntity = inventoryRepository.findById(productId).orElse(null);
+
+        // 새로운 제품 등록할 때
+        if(inventoryEntity == null){
+            inventoryEntity = InventoryEntity.builder()
+                    .productId(productId)
+                    .quantity(qty)
+                    .createdAt(Instant.now())
+                    .build();
+            inventoryRepository.save(inventoryEntity);
+        // 기존에 있던 제품 등록할 때
+        }else{
+            inventoryEntity.setQuantity(inventoryEntity.getQuantity() + qty);
+            inventoryEntity.setUpdatedAt(Instant.now());
+        }
+        inventoryRepository.save(inventoryEntity);
     }
 
     @Transactional(readOnly = true)
-    public int getQty(String productId) {
-        return repo.findById(productId).map(InventoryEntity::getQuantity).orElse(0);
+    public InventoryResponseDto getInventory(String productId){
+        InventoryEntity inventoryEntity = inventoryRepository.findById(productId)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "Dose not exist = " + productId
+                ));
+        return new InventoryResponseDto(
+                inventoryEntity.getProductId(),
+                inventoryEntity.getQuantity(),
+                inventoryEntity.getCreatedAt(),
+                inventoryEntity.getUpdatedAt()
+        );
     }
 
     @Transactional
-    public void reserve(String productId, int qty) {
-        InventoryEntity e = repo.findById(productId)
-                .orElseThrow(() -> new IllegalArgumentException("product not found: " + productId));
+    public void reserve(String productId, int qty){
+        var e = inventoryRepository.findById(productId).orElseThrow(() -> new IllegalArgumentException("product not found: "+productId));
         if (e.getQuantity() < qty) throw new IllegalStateException("not enough stock");
-        e.setQuantity(e.getQuantity() - qty);
-        e.setUpdatedAt(Instant.now());
+        e.setQuantity(e.getQuantity() - qty); e.setUpdatedAt(Instant.now());
     }
 
     @Transactional
-    public void release(String productId, int qty) {
-        InventoryEntity e = repo.findById(productId)
-                .orElseThrow(() -> new IllegalArgumentException("product not found: " + productId));
-        e.setQuantity(e.getQuantity() + qty);
-        e.setUpdatedAt(Instant.now());
+    public void release(String productId, int qty){
+        var e = inventoryRepository.findById(productId).orElseThrow(() -> new IllegalArgumentException("product not found: "+productId));
+        e.setQuantity(e.getQuantity() + qty); e.setUpdatedAt(Instant.now());
     }
 }
