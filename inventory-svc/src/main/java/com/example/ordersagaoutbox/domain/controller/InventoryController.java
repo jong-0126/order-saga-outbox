02cd2bdf25d4bd3ab136587555bd5fbee1680986
baegjonghyeon <baek011226@naver.com>
2025-08-18 17:34:01 +0900

    feat: inventoryController - 시드 추가 - 시드 조회

--- inventory-svc/src/main/java/com/example/ordersagaoutbox/domain/controller/InventoryController.java
+++ inventory-svc/src/main/java/com/example/ordersagaoutbox/domain/controller/InventoryController.java
@@ -1,7 +1,9 @@
 package com.example.ordersagaoutbox.domain.controller;
 
+import com.example.ordersagaoutbox.domain.dto.InventoryResponseDto;
 import com.example.ordersagaoutbox.domain.service.InventoryService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,31 +13,28 @@ import java.util.Map;
 @RequiredArgsConstructor
 @RequestMapping("/inventory")
 public class InventoryController {
-    private final InventoryService service;
+    private final InventoryService inventoryService;
 
-    // 재고 시드 (테스트용)
     @PostMapping("/seed")
-    public ResponseEntity<Void> seed(@RequestParam String productId, @RequestParam int qty) {
-        service.seed(productId, qty);
+    public ResponseEntity<Void> seed(
+            @RequestParam("productId") String productId,
+            @RequestParam("qty") int qty
+    ){
+        inventoryService.seed(productId, qty);
         return ResponseEntity.ok().build();
     }
-
-    // 수량 조회
     @GetMapping("/{productId}")
-    public Map<String, Object> get(@PathVariable String productId) {
-        return Map.of("productId", productId, "qty", service.getQty(productId));
+    public ResponseEntity<InventoryResponseDto> get(
+            @PathVariable("productId") String productId
+    ){
+        return ResponseEntity.ok(inventoryService.getInventory(productId));
     }
-
-    // 수동 예약/해제(테스트 편의용)
     @PostMapping("/reserve")
-    public ResponseEntity<Void> reserve(@RequestParam String productId, @RequestParam int qty) {
-        service.reserve(productId, qty);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<Void> reserve(@RequestParam("productId") String productId, @RequestParam("qty") int qty){
+        inventoryService.reserve(productId, qty); return ResponseEntity.ok().build();
     }
-
     @PostMapping("/release")
-    public ResponseEntity<Void> release(@RequestParam String productId, @RequestParam int qty) {
-        service.release(productId, qty);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<Void> release(@RequestParam("productId") String productId, @RequestParam("qty") int qty){
+        inventoryService.release(productId, qty); return ResponseEntity.ok().build();
     }
 }
